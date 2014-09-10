24aeb1425cda7b7edcecaa55489b9bcccb0e3548
rebeccafransson <rf222cz@student.lnu.se>
2014-09-10 14:14:46 +0200

    Done with readoddbyte

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,20 +10,61 @@ namespace Ritamedasterisker
     {
         static void Main(string[] args)
         {
-            uint input; //http://www.c-sharpcorner.com/UploadFile/2072a9/triangle-patterns-in-C-Sharp/ triangel
+            byte oddNumber; //triangel http://sharepoint2010mind.blogspot.se/2012/10/c-program-to-print-patterns-of-numbers.html
 
             do
             {
-
+                oddNumber = ReadOddByte();
+                RenderTriangle(oddNumber);
 
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
         }
         static byte ReadOddByte()
         {
+            byte value = 0; //In this methos we check if the input number is a odd number and less than 79. If its not, there vill be a catch
+            string input;
+            while (true)
+            {
+                Console.Write("Skriv in ett udda tal som är mindre än 79: ");
+                input = Console.ReadLine();
+                try
+                {
+                    value = byte.Parse(input);
+                    if (value % 2 != 0 && value <= 79) //If value a odd number and less than 79, then break!               ??? hur value <=79?????
+                    {
+                        break;
+                    }
+                    throw new Exception();
+                }
+                catch //if the for doesnt work, then the catch will come up
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ditt tal '{0}' fungerar inte. Vänligen skriv ett udda tal som är mindre än 79.", input);
+                    Console.ResetColor();
+                }
+                
 
-
+            }
+            return value;
         }
 
+        static void RenderTriangle(byte cols)
+        {
+
+            int oddNumber,c,n,temp;
+            n = 5;
+           temp= n ;
+           for (oddNumber = 1; oddNumber <= n; oddNumber++)
+           {
+               for (c = 1; c < temp; c++)
+                   Console.Write(" ");
+               
+               temp--;
+               for (c = 1; c <= 2 * oddNumber - 1; c++)
+                   Console.Write("*");
+
+               Console.WriteLine();
+        }
     }
 }
