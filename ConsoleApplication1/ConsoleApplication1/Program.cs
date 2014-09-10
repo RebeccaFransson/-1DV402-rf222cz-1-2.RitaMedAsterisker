91ddc6bfe16733c74e889437f9ce03b7d6cf8e16
rebeccafransson <rf222cz@student.lnu.se>
2014-09-10 10:02:34 +0200

    påbörjad lab 2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ritamedasterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint input; //http://www.c-sharpcorner.com/UploadFile/2072a9/triangle-patterns-in-C-Sharp/ triangel
+
+            do
+            {
+
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+        }
+        static byte ReadOddByte()
+        {
+
+
+        }
+
+    }
+}
